311931d290a5f281320673282680470a0804c62c
Rezin Anton <rezin.anton@gmail.com>
2023-10-10 21:46:24 +0300

    Delete Functionality

--- src/main/java/ProjectBookStore/Bookstore/controller/BookController.java
+++ src/main/java/ProjectBookStore/Bookstore/controller/BookController.java
@@ -3,19 +3,26 @@ package ProjectBookStore.Bookstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import ProjectBookStore.Bookstore.domain.BookStoreRepository;
 
 @Controller
 public class BookController {
-	@Autowired
-	private BookStoreRepository repository;
+    @Autowired
+    private BookStoreRepository repository;
 
-	@RequestMapping("/booklist")
-	public String bookList(Model model) {
-		model.addAttribute("books", repository.findAll());
+    @RequestMapping(value = { "/", "/addbook" })
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "addbook";
+    }
 
-		return "booklist";
-	}
-	
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        repository.deleteById(bookId);
+        return "redirect:/addbook";
+    }
 }
